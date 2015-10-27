b470b08dc9d09fba965d83ab3ea625ded112d866
Petr Kalambet <peter@technogeek.io>
2015-10-27 14:49:47 +0300

    Because of Bluemix integration get rid of the internal package 'service'

--- app.go
+++ app.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/kalambet/mission-control/service"
 	//for extracting service credentials from VCAP_SERVICES
 	//"github.com/cloudfoundry-community/go-cfenv"
 )
@@ -15,6 +14,45 @@ import (
 const (
 	DEFAULT_PORT = "8080"
 )
+
+// Service we are going to monitor
+type Service struct {
+	HostName string
+	Name     string
+}
+
+// GetName returns Service friendly Name
+func (s Service) GetName() string {
+	return s.Name
+}
+
+// GetStatus returns Service current status
+func (s Service) GetStatus() bool {
+	resp, err := http.Get(s.HostName)
+	if err != nil {
+		fmt.Printf("\nError getting %s service: %s\n", s.HostName, err)
+		return false
+	}
+
+	if code := resp.StatusCode; code != 401 && code != 200 {
+		fmt.Printf("\nError getting %s service, response status is %d\n", s.HostName, resp.StatusCode)
+		return false
+	}
+
+	return true
+}
+
+// CreateServiceList creates default service list
+func CreateServiceList() (list []Service) {
+	list = append(list, Service{Name: "Authorization", HostName: "https://idmx-service-authz.mybluemix.net"})
+	list = append(list, Service{Name: "Authentication", HostName: "https://idmx-service-authn.mybluemix.net"})
+	list = append(list, Service{Name: "Directory", HostName: "https://idmx-service-directory.mybluemix.net"})
+	list = append(list, Service{Name: "Issuance", HostName: "https://idmx-service-issuing.mybluemix.net"})
+	list = append(list, Service{Name: "Media", HostName: "https://idmx-service-media.mybluemix.net/docs/idemix.json"})
+	list = append(list, Service{Name: "ID Service", HostName: "https://idmx-service-wallet.mybluemix.net"})
+	list = append(list, Service{Name: "Service Broker", HostName: "https://idmx-service.mybluemix.net"})
+	return
+}
 
 var index = template.Must(template.ParseFiles(
 	"templates/_base.html",
@@ -30,7 +68,7 @@ func helloworld(w http.ResponseWriter, req *http.Request) {
 }
 
 func handleStatusRequest(w http.ResponseWriter, r *http.Request) {
-	error := table.Execute(w, service.CreateServiceList())
+	error := table.Execute(w, CreateServiceList())
 	if error != nil {
 		fmt.Printf("\nError during template formation: %s\n", error)
 	}
